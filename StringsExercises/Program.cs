bd8f6a05b245aaf978a5dd9217cf1b3de38fffff
Jarosław Bagnicki <jbagnicki@future-processing.com>
2019-07-26 13:31:23 +0200

    Parsing time

--- StringsExercises/Program.cs
+++ StringsExercises/Program.cs
@@ -7,7 +7,8 @@ namespace StringsExercises
         static void Main(string[] args)
         {
             //ConsecutiveTest.Run();
-            Duplicates.Run();
+            //Duplicates.Run();
+            TimeParsing.Run();
         }
     }
 }
---
+++ StringsExercises/TimeParsing.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace StringsExercises
+{
+    public class TimeParsing
+    {
+        public static void Run()
+        {
+            string input = GetTime();
+
+            bool isValidTime = ParseTime(input);
+
+            Console.WriteLine(isValidTime ? "Ok." : "Invalid Time.");
+
+            Run();
+        }
+
+        private static bool ParseTime(string input)
+        {
+            Regex timeRx = new Regex(@"^$|^((0?[0-9])|(1[0-9])|(2[0-3])):[0-5][0-9]$");
+            return timeRx.IsMatch(input);
+        }
+
+        private static string GetTime()
+        {
+            while (true)
+            {
+                Console.Write("Enter time: ");
+                string input = Console.ReadLine();
+
+                if (String.IsNullOrWhiteSpace(input))
+                    continue;
+                return input;
+            }
+        }
+    }
+}
